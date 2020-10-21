1ce9cea577a25836bc059579f930dfbd84700648
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-20 22:17:33 -0500

    [ADD] error middleware added

---
+++ api/src/middlewares/error.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { TError, defaultError } from '../utils/constants';
+
+const errorMiddleware = (
+  err: TError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const statusCode: number = err.statusCode || defaultError.statusCode;
+
+  res.status(statusCode).json({
+    id: err.id || defaultError.id,
+    statusCode,
+    message: err.message || defaultError.message,
+  });
+};
+
+export default errorMiddleware;
