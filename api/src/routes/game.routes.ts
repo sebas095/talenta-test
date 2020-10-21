a2104e3af8b23bb67dd36fd7751244990fba10a6
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-21 04:37:08 -0500

    [ADD] stats, update routes in game added

--- api/src/routes/game.routes.ts
+++ api/src/routes/game.routes.ts
@@ -4,12 +4,15 @@ import { container } from '../config/container';
 
 const gameController: GameController = container.resolve('GameController');
 
-export default (app: Router, mountPoint: string) => {
+export default (app: Router, mountPoint: string): void => {
   const router: Router = Router();
 
   // routes
   router.get('/', gameController.getAll);
+  router.get('/stats', gameController.stats);
   router.get('/:gameId', gameController.get);
+
+  router.patch('/:gameId', gameController.update);
 
   app.use(mountPoint, router);
 };
