44535cb4259740c58f1c594a7f174e4d42261c0d
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-21 22:32:49 -0500

    [ADD] layout component added

---
+++ client/src/components/layout/layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import PlayersContainer from '../../containers/players-container';
+
+const LayoutStyled = styled.div`
+  width: 100%;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 330px 1fr;
+  grid-template-areas: 'started game finished';
+  width: 100%;
+`;
+
+const Layout = ({ children }) => {
+  return (
+    <LayoutStyled>
+      <PlayersContainer />
+      <Container>{children}</Container>
+    </LayoutStyled>
+  );
+};
+
+export default Layout;
