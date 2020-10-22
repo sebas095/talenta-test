65f526046bbe07b5effdb152cd8a826214cdddf7
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-21 22:32:05 -0500

    [ADD] player component added

---
+++ client/src/components/game/player.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from '../../styles/colors';
+
+const PlayerStyled = styled.div`
+  width: 250px;
+  padding: 0.5rem;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: white;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  border-radius: 20px;
+  border-bottom: ${({ active }) =>
+    active ? `solid 5px ${colors.mainColor}` : 'none'};
+  cursor: pointer;
+
+  .players__score {
+    color: ${colors.secondary};
+  }
+`;
+
+const Player = ({ score, player, active }) => {
+  return (
+    <PlayerStyled active={active}>
+      <div className="players__player">{player}</div>
+      <div className="players__score">{score > 0 ? score : '-'}</div>
+    </PlayerStyled>
+  );
+};
+
+export default Player;
