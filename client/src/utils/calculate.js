765bd9754ff40c1e6974c8a003b1c25b803b17bf
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-22 03:54:24 -0500

    [ADD] utils added

---
+++ client/src/utils/calculate.js
@@ -0,0 +1,42 @@
+const lines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+export function calculateWinner(squares) {
+  for (let i = 0; i < lines.length; i += 1) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export function calculateWinnerIndex(squares) {
+  for (let i = 0; i < lines.length; i += 1) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return [a, b, c];
+    }
+  }
+  return [null];
+}
+
+export function isDraw(squares) {
+  const hasWinner = !!calculateWinner(squares);
+  return !hasWinner && !squares.includes(null);
+}
+
+export function indexToRowCol(index, width) {
+  const row = Math.floor(index / width);
+  const col = index % width;
+
+  return [row, col];
+}
