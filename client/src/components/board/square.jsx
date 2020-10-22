830729d8fc9beec295f3d21cc92acf2831415279
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-21 22:31:26 -0500

    [ADD] square component added

---
+++ client/src/components/board/square.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import colors from '../../styles/colors';
+
+const SquareStyled = styled.div`
+  text-align: center;
+  font-size: 5rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: ${({ color }) => color || '#000'};
+  position: relative;
+  cursor: pointer;
+
+  &:nth-child(3n - 1) {
+    border-right: 6px solid ${colors.secondary};
+    border-left: 6px solid ${colors.secondary};
+  }
+
+  &:nth-child(n + 4) {
+    border-top: 6px solid ${colors.secondary};
+  }
+`;
+
+const Board = ({ player }) => (
+  <SquareStyled color={player === 'X' ? 'black' : colors.mainColor}>
+    {player}
+  </SquareStyled>
+);
+
+export default Board;
