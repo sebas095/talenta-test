e0b1ec4d157fd58a21a9708d04f65630fe6cc05b
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-22 04:56:58 -0500

    [ADD] api service added

---
+++ client/src/services/api.service.js
@@ -0,0 +1,72 @@
+import defaultError from '../utils/constants';
+
+const { API_ENDPOINT } = process.env;
+
+class ApiService {
+  static async getStats() {
+    try {
+      const response = await fetch(`${API_ENDPOINT}/game/stats`);
+      const stats = await response.json();
+      return stats;
+    } catch (err) {
+      return defaultError;
+    }
+  }
+
+  static async listGames() {
+    try {
+      const response = await fetch(`${API_ENDPOINT}/game`);
+      const games = await response.json();
+      return games;
+    } catch (err) {
+      return defaultError;
+    }
+  }
+
+  static async getGame(gameId) {
+    try {
+      const response = await fetch(`${API_ENDPOINT}/game/${gameId}`);
+      const game = await response.json();
+      return game;
+    } catch (err) {
+      return defaultError;
+    }
+  }
+
+  static async createGame(body) {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    };
+
+    try {
+      const response = await fetch(`${API_ENDPOINT}/game`, requestOptions);
+      const game = await response.json();
+      return game;
+    } catch (err) {
+      return defaultError;
+    }
+  }
+
+  static async updateGame(gameId, body) {
+    const requestOptions = {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    };
+
+    try {
+      const response = await fetch(
+        `${API_ENDPOINT}/game/${gameId}`,
+        requestOptions,
+      );
+      const game = await response.json();
+      return game;
+    } catch (err) {
+      return defaultError;
+    }
+  }
+}
+
+export default ApiService;
