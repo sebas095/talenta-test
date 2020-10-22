e79b6902589850d98c71c724e3cf48c985c5da57
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-22 03:42:05 -0500

    [ADD] next function added in error middleware

--- api/src/middlewares/error.middleware.ts
+++ api/src/middlewares/error.middleware.ts
@@ -1,14 +1,23 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { TError, defaultError } from '@utils/constants';
 
-const errorMiddleware = (err: TError, req: Request, res: Response): void => {
-  const statusCode: number = err.statusCode || defaultError.statusCode;
+const errorMiddleware = (
+  err: TError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  if (err) {
+    const statusCode: number = err.statusCode || defaultError.statusCode;
 
-  res.status(statusCode).json({
-    id: err.id || defaultError.id,
-    statusCode,
-    message: err.message || defaultError.message,
-  });
+    res.status(statusCode).json({
+      id: err.id || defaultError.id,
+      statusCode,
+      message: err.message || defaultError.message,
+    });
+  } else {
+    next();
+  }
 };
 
 export default errorMiddleware;
