9da91300aad85165129cdc4c5019ec845c011c8d
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-22 17:57:38 -0500

    [REF] import scale animation refactored

--- client/src/components/common/modal.jsx
+++ client/src/components/common/modal.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import styled from 'styled-components';
 
-import scaleAnimation from '@styles/keyFrames';
+import { scaleAnimation } from '@styles/keyFrames';
 
 const modalRoot = document.getElementById('modal');
 
