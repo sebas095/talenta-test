cbb8901dbba90e032d1afe5468eae0ef81b0908b
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-22 18:58:56 -0500

    [ADD] pause container in players container

--- client/src/containers/players-container.jsx
+++ client/src/containers/players-container.jsx
@@ -5,19 +5,30 @@ import Player from '@components/game/player';
 import Turn from '@components/game/turn';
 import Alert from '@components/common/alert';
 
+import PauseContainer from './pause-container';
+
 const PlayersContainerStyled = styled.div`
   width: 100%;
   margin-top: 30px;
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: center;
   flex-wrap: wrap;
+`;
+
+const ContainerStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const PlayersContainer = () => (
   <PlayersContainerStyled>
-    <Player score={0} player="X" active />
-    <Player score={0} player="O" />
+    <ContainerStyled>
+      <Player score={0} player="X" active />
+      <PauseContainer />
+      <Player score={0} player="O" />
+    </ContainerStyled>
     <Turn player="X" text="Comenzar partida o seleccionar jugador " />
     <Alert message="El recurso no existe o no tienes privilegios para acceder. ID invalido" />
   </PlayersContainerStyled>
