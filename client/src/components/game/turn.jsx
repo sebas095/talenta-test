85943546b7ce169d100b2d3132528024204d8b4f
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-23 03:19:21 -0500

    [ADD] fade animation added in turn component

--- client/src/components/game/turn.jsx
+++ client/src/components/game/turn.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+
 import colors from '@styles/colors';
+import { fadeAnimation } from '@styles/keyFrames';
 
 const TurnStyled = styled.div`
   width: 100%;
   text-align: center;
   font-size: 1.5rem;
   padding-top: 1rem;
+  animation: ${fadeAnimation} 0.7s ease;
 
   span {
     font-weight: bold;
