1e4f21f222f08f634d72a1d8800bab3709790b9f
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-23 02:05:41 -0500

    [ADD] hooks added in games started container

--- client/src/containers/games-started-container.jsx
+++ client/src/containers/games-started-container.jsx
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 
 import GameList from '@components/game/game-list';
 
-import ApiService from '@services/api.service';
-
 import { GameContext } from '@context/game';
 import { GamesContext } from '@context/games';
 import { ErrorContext } from '@context/error';
 import { LoadingContext } from '@context/loading';
+
+import useGetGame from '@hooks/useGetGame';
+import useListGames from '@hooks/useListGames';
 
 const GamesStartedContainerStyled = styled.div`
   width: 100%;
@@ -22,30 +23,14 @@ const GamesStartedContainer = () => {
   const { game, setGame } = useContext(GameContext);
   const { games, setGames } = useContext(GamesContext);
   const { setError } = useContext(ErrorContext);
-  const { loading } = useContext(LoadingContext);
+  const { loading, setLoading } = useContext(LoadingContext);
+
+  const fetchGame = useGetGame({ game, setGame, setError });
+  const fetchGames = useListGames({ setLoading, setError, setGames, game });
 
   const showGame = id => {
     setGame({ ...game, gameId: id });
-  };
-
-  const fetchGame = async () => {
-    const data = await ApiService.getGame(game.gameId);
-    if (data.message) {
-      setError(data.message);
-    } else {
-      setGame({ ...game, data });
-    }
-  };
-
-  const fetchGames = async () => {
-    const data = await ApiService.listGames();
-    if (data.message) {
-      setError(data.message);
-    } else if (data.started[0]._id === game.gameId) {
-      setGames({ ...data, started: data.started.slice(1) });
-    } else {
-      setGames(data);
-    }
+    localStorage.setItem('gameId', id);
   };
 
   useEffect(() => {
