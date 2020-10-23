8529f49e05490c7043efae32337a8490e436fde3
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-22 23:06:16 -0500

    [ADD] default value in game context

--- client/src/context/game.js
+++ client/src/context/game.js
@@ -3,7 +3,11 @@ import React, { useState, createContext } from 'react';
 export const GameContext = createContext({});
 
 export const GameContextProvider = ({ children }) => {
-  const [game, setGame] = useState({});
+  const [game, setGame] = useState({
+    board: Array(9).fill(null),
+    gameId: '',
+    winner: '',
+  });
 
   return (
     <GameContext.Provider value={{ game, setGame }}>
