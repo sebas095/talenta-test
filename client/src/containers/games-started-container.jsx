fdc0502df10ba386678eabe3b3d814941ab4b535
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-22 23:16:03 -0500

    [ADD] fetch games and game in games started container

--- client/src/containers/games-started-container.jsx
+++ client/src/containers/games-started-container.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import styled from 'styled-components';
 
-import GameList from '../components/game/game-list';
+import GameList from '@components/game/game-list';
+
+import ApiService from '@services/api.service';
+
+import { GameContext } from '@context/game';
+import { GamesContext } from '@context/games';
+import { ErrorContext } from '@context/error';
+import { LoadingContext } from '@context/loading';
 
 const GamesStartedContainerStyled = styled.div`
   width: 100%;
@@ -11,44 +18,54 @@ const GamesStartedContainerStyled = styled.div`
   grid-area: started;
 `;
 
-const games = {
-  started: [
-    {
-      _id: 'ljhljl',
-      turn: 'X',
-    },
-    {
-      _id: 'asdd',
-      turn: 'O',
-    },
-    {
-      _id: 'rtyrt',
-      turn: 'X',
-    },
-    {
-      _id: 'rtyh',
-      turn: 'X',
-    },
-    {
-      _id: 'poks',
-      turn: 'O',
-    },
-    {
-      _id: 'lklkb7',
-      turn: 'X',
-    },
-  ],
-};
+const GamesStartedContainer = () => {
+  const { game, setGame } = useContext(GameContext);
+  const { games, setGames } = useContext(GamesContext);
+  const { setError } = useContext(ErrorContext);
+  const { loading } = useContext(LoadingContext);
 
-const GamesStartedContainer = () => (
-  <GamesStartedContainerStyled>
-    <GameList
-      games={games.started}
-      title="Iniciados"
-      height="400px"
-      scrollOrientation="rtl"
-    />
-  </GamesStartedContainerStyled>
-);
+  const showGame = id => {
+    setGame({ ...game, gameId: id });
+  };
+
+  const fetchGame = async () => {
+    const data = await ApiService.getGame(game.gameId);
+    if (data.message) {
+      setError(data.message);
+    } else {
+      setGame({ ...game, data });
+    }
+  };
+
+  const fetchGames = async () => {
+    const data = await ApiService.listGames();
+    if (data.message) {
+      setError(data.message);
+    } else if (data.started[0]._id === game.gameId) {
+      setGames({ ...data, started: data.started.slice(1) });
+    } else {
+      setGames(data);
+    }
+  };
+
+  useEffect(() => {
+    fetchGame();
+    fetchGames();
+  }, [game.gameId, game.winner]);
+
+  return (
+    <GamesStartedContainerStyled>
+      <GameList
+        games={games.started}
+        title="Iniciados"
+        height="400px"
+        scrollOrientation="rtl"
+        showGame={showGame}
+        loading={loading}
+        message="No hay juegos pendientes..."
+      />
+    </GamesStartedContainerStyled>
+  );
+};
 
 export default GamesStartedContainer;
