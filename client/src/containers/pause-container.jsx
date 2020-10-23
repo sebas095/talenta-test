c763b490a4d1cfa975902f445e6077bae1d7768e
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-23 06:04:13 -0500

    [ADD] handle new game in pause container

--- client/src/containers/pause-container.jsx
+++ client/src/containers/pause-container.jsx
@@ -2,20 +2,10 @@ import React, { useState, useContext } from 'react';
 import Pause from '@components/game/pause';
 
 import { GameContext } from '@context/game';
-import { ErrorContext } from '@context/error';
-
-import useCreateGame from '@hooks/useCreateGame';
 
 const PauseContainer = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { setError } = useContext(ErrorContext);
-  const { game, setGame } = useContext(GameContext);
-  const createGame = useCreateGame({
-    setError,
-    setGame,
-    game,
-    body: Array(9).fill(null),
-  });
+  const { setGame } = useContext(GameContext);
 
   const toggleModal = () => {
     setIsOpen(!isOpen);
@@ -23,7 +13,8 @@ const PauseContainer = () => {
 
   const handleNewGame = () => {
     setIsOpen(false);
-    createGame();
+    localStorage.removeItem('gameId');
+    setGame({ board: Array(9).fill(null), winner: '' });
   };
 
   return (
