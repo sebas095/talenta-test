df15437ba988cbb8e9f5ef64b148ee3ab0a377dd
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-23 02:05:09 -0500

    [ADD] hooks added in pause container

--- client/src/containers/pause-container.jsx
+++ client/src/containers/pause-container.jsx
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Pause from '@components/game/pause';
+
+import { GameContext } from '@context/game';
+import { ErrorContext } from '@context/error';
+import { LoadingContext } from '@context/loading';
+
+import useCreateGame from '@hooks/useCreateGame';
 
 const PauseContainer = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { setError } = useContext(ErrorContext);
+  const { setLoading } = useContext(LoadingContext);
+  const { game, setGame } = useContext(GameContext);
+  const createGame = useCreateGame({
+    setLoading,
+    setError,
+    setGame,
+    game,
+    body: Array(9).fill(null),
+  });
 
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
 
-  return <Pause isOpen={isOpen} toggleModal={toggleModal} />;
+  const handleNewGame = () => {
+    setIsOpen(false);
+    createGame();
+  };
+
+  return (
+    <Pause
+      isOpen={isOpen}
+      toggleModal={toggleModal}
+      handleNewGame={handleNewGame}
+    />
+  );
 };
 
 export default PauseContainer;
