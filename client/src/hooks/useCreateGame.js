5075e4969baeccb5db1799cbb802b4b90ed1cf2f
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-23 06:01:19 -0500

    [REF] use create game hook refactored

--- client/src/hooks/useCreateGame.js
+++ client/src/hooks/useCreateGame.js
@@ -1,13 +1,12 @@
 import ApiService from '@services/api.service';
 
-const useCreateGame = ({ setError, setGame, game, body }) => async () => {
-  const data = await ApiService.createGame(body || game);
-
+const useCreateGame = ({ setError, setGame, game }) => async () => {
+  const data = await ApiService.createGame({ board: Array(9).fill(null) });
   if (data.message) {
     setError(data.message);
   } else {
-    setGame({ ...game, ...data, gameId: data._id });
     localStorage.setItem('gameId', data._id);
+    setGame({ ...game, ...data, gameId: data._id });
   }
 };
 
