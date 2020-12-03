8c5c968a33e8595c09f6d99d6c28c92a51b8a833
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-12-03 03:01:44 -0500

    [ADD] jest mongodb config

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -1,14 +1,19 @@
 import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  transform: { '^.+\\.tsx?$': 'ts-jest' },
+  preset: '@shelf/jest-mongodb',
   moduleNameMapper: {
     '@models': '<rootDir>/src/models',
+    '@routes': '<rootDir>/src/routes',
     '@services': '<rootDir>/src/services',
+    '@controllers': '<rootDir>/src/controllers',
+    '@middlewares': '<rootDir>/src/middlewares',
     '@repositories': '<rootDir>/src/repositories',
     '@utils/(.*)': '<rootDir>/src/utils/$1',
+    '@config/(.*)': '<rootDir>/src/config/$1',
     '@mocks': '<rootDir>/tests/mocks',
+    '@app': '<rootDir>/index.ts',
   },
 };
 
